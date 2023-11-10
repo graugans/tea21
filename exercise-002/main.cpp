9b8949c486b374c075cced463f4142e73fcfbe67
Thieser <149794167+Thieser@users.noreply.github.com>
2023-11-10 10:10:57 +0100

    Counter added

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -10,17 +10,19 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
+    int count =20;
     CLI::App app{PROJECT_NAME};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, "Variable count");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
-
+    printf("%d",count);
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
@@ -29,6 +31,5 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
-
     return 0; /* exit gracefully*/
 }
