9965e9cdc5562022da2bed5806c4e9741b8b1e71
DanielWolf2 <149794466+DanielWolf2@users.noreply.github.com>
2023-11-10 10:23:36 +0100

    Aenderungen eingefuegt

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -4,6 +4,7 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+
 auto main(int argc, char **argv) -> int
 {
     /**
@@ -11,22 +12,28 @@ auto main(int argc, char **argv) -> int
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
     CLI::App app{PROJECT_NAME};
+    int count = 20;
     try
     {
+
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, "Variable count als Integer");
         app.parse(argc, argv);
+
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    std::vector<int> data(count);
 
+    fmt::print("Created a vector with {} elements\n", data.size());
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("Hello, {}!\n", count, app.get_name());
 
     /* INSERT YOUR CODE HERE */
 
