256c177760f3bc83c2f505e5c8e72ee3eba6196a
MKrudi <149794200+MKrudi@users.noreply.github.com>
2023-12-01 16:14:12 +0100

    Update Bro

--- exercise-005/main.cpp
+++ exercise-005/main.cpp
@@ -3,6 +3,75 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
+#include <chrono>
+#include <thread>
+
+void sleep_100_ms() {
+    std::chrono::milliseconds duration(100);
+    std::this_thread::sleep_for(duration);
+};
+
+
+int getInput(void)
+{
+    int number;
+    std::cout << "Geben Sie eine Zahl ein: ";
+    std::cin >> number;
+    std::cout << number << std::endl;
+    return number;
+};
+
+typedef enum {
+    STATE_Up = 1,
+    STATE_Down = 2,
+    STATE_Off = 0,
+} GarageState;
+
+GarageState state = STATE_Off;
+
+void stateMachine()
+{
+    int input = getInput();
+    switch(state)
+    {
+        case STATE_Off:
+            if(6 == input)
+            {
+                state = STATE_Up;
+                std::cout << state << std::endl;
+            }
+            else if (5 == input)
+            {
+                state = STATE_Down;
+                std::cout << state << std::endl;
+            }
+        break;
+        case STATE_Up:
+            if (4 == input)
+            {
+                state = STATE_Off;
+                std::cout << state << std::endl;
+            }
+            else if (7 == input)
+            {
+                state = STATE_Down;
+                std::cout << state << std::endl;
+            }
+        break;
+        case STATE_Down:
+            if (3 == input)
+            {
+                state = STATE_Off;
+                std::cout << state << std::endl;
+            }
+            else if (7 == input)
+            {
+                state = STATE_Down;
+                std::cout << state << std::endl;
+            }
+        break;
+    }
+}
 
 auto main(int argc, char **argv) -> int
 {
@@ -21,14 +90,11 @@ auto main(int argc, char **argv) -> int
         return app.exit(e);
     }
 
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", app.get_name());
-
-    /* INSERT YOUR CODE HERE */
+    while (true)
+    {
+        stateMachine();
+        sleep_100_ms();
+    }
 
     return 0; /* exit gracefully*/
 }
